a517471fcbe60e52b19ff5e1baf22ee209a3afc8
rennyallen <rennyallen@hotmail.com>
2017-10-14 21:19:45 +0800

    fix

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,6 +1,7 @@
 //= require jquery-1.7.1
 
 $(function(){
+  $('.modal-overlay').hide();
   $('.CompanyDropdown-module__dropdownWrapper___a38').click(function(){
     $('.CompanyMenu-module__childMenu___2e3').toggleClass('show');
   });
@@ -10,16 +11,16 @@ $(function(){
   });
 
   $('.ServiceDetailView-module__button___ce7').click(function(){
+    step = 1;
+    $('.modal-overlay').removeClass('hide');
     $('.modal-overlay').show();
   })
 
-  $('.close-modal-btn').click(function(){
+  $('body').on('click', '.close-modal-btn', function(){
     $('.modal-overlay').hide();
   })
 
-  var step = 0;
   $('.button-next').click(function(){
-    step += 1;
     $('.service-request-form-content').empty();
     switch(step){
       case 1:
@@ -34,7 +35,14 @@ $(function(){
         $('.service-request-form-content').html('<div class="SchedulingForm-module__schedulingForm___53e"><div class="SchedulingForm-module__radioGroup___4a0"><div class="RadioButton__wrapper___3R4 RadioButton__medium___1IX SchedulingForm-module__radioButton___5ef SchedulingForm-module__radioGroup___4a0-radiobutton"><input id="radio-0417323601663730" name="urgency" value="urgent" type="radio"><label for="radio-0417323601663730">I need it done in the next 24-48 hours</label></div><div class="RadioButton__wrapper___3R4 RadioButton__medium___1IX SchedulingForm-module__radioButton___5ef SchedulingForm-module__radioGroup___4a0-radiobutton"><input id="radio-8395763394606155" name="urgency" value="specific_date" type="radio"><label for="radio-8395763394606155">I have a specific time and date in mind</label></div><div class="RadioButton__wrapper___3R4 RadioButton__medium___1IX SchedulingForm-module__radioButton___5ef SchedulingForm-module__radioGroup___4a0-radiobutton"><input id="radio-5361744349553449" name="urgency" value="flexible" type="radio"><label for="radio-5361744349553449">I\'m flexible</label></div><div class="RadioButton__wrapper___3R4 RadioButton__medium___1IX SchedulingForm-module__radioButton___5ef SchedulingForm-module__radioGroup___4a0-radiobutton"><input id="radio-1531705377604815" name="urgency" value="price_only" type="radio"><label for="radio-1531705377604815">I\'m just looking for a price</label></div></div></div>');
         break;
       case 3:
+        $('.question-text').text('Anything else we should know (COI requirements, logistics, etc.)?');
+        $('.service-request-form-content').html('<div class="service-request-form-question"><div class="Textarea__wrapper___TXm"><textarea class="Textarea__textarea___1HD Textarea__medium___21n" name="additionalComment" rows="5" placeholder="Your comment..." data-qa="additional-comment"></textarea></div></div>');
+        $(this).text('Submit');
+        break;
+      case 4:
+        $('.modal-overlay').html('<div class="modal-container"><i class="close-modal-btn" data-qa="close-modal-btn"></i><div class="service-request-form-success"><div class="header-padding"></div><div class="order-received-image"></div><div class="title">Your order has been received</div><div class="description">Our Project Managers are reviewing your request and will send you quotes to review soon</div><div class="footer"><button class="Button__button___1G9 Button__medium___21S Button__blue___mIz view-order-button" data-qa="service-request-form-view-order" name="button"><!-- react-text: 4156 -->View Order<!-- /react-text --></button></div></div></div>');
         break;
      }
+    step += 1;
   })
 });
